18e511da6492e4ad86e4125c08a7e867b190bf8a
Ali <nic.oleda.l.i.rae@gmail.com>
2024-05-31 18:03:10 +0300

    make it better

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import time 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, طابت ياقحبة"
+    time.sleep(200) 
+    return "Hello, اتحداك تشغله"
 
 if __name__ == '__main__':
     app.run(debug=True)
