6ccc39e5fb983ef739c7649b59d0c97d31baad97
Ali <nic.oleda.l.i.rae@gmail.com>
2024-06-03 20:15:50 +0300

    make it better

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 import time 
-
+import request
 app = Flask(__name__)
 
+listd={}
+
+def c(user):
+  listd[user]=1 
+def g():
+  return len(listd)
 @app.route('/')
 def home():
-    time.sleep(200) 
-    return "Hello, اتحداك تشغله"
-
+    return g()#"Hello, اتحداك تشغله"
+@app.route('/add', methods=['POST'])
+def add():
+  data = request.get_json()
+  u = data["user"]
+  c(u)
+  return "ok"
 if __name__ == '__main__':
     app.run(debug=True)
